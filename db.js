e7a552317dc8931fd635b0a50c61b3da3bcfcfdc
Vladislav Zubov <vladislav.zubov.20@gmail.com>
2021-11-12 12:19:52 +0300

    added connection url

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME,DATABASE_URL } = process.env
 
 const knex = require('knex')({
   client: 'postgres',
-  connection: {
+  connection: DATABASE_URL ? DATABASE_URL : {
     host : DB_HOST,
     port : DB_PORT,
     user : DB_USER,
