be63165bc5460cc47f5d98b8e52c49dfe97fe171
Vladislav Zubov <vladislav.zubov.20@gmail.com>
2021-11-12 12:26:33 +0300

    updated connection url

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME,DATABASE_URL } = process.env
 
-const knex = require('knex')({
+const knex = DATABASE_URL ? require('knex')({
+  client: 'pg',
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: false
+  }
+}) :  require('knex')({
   client: 'postgres',
-  connection: DATABASE_URL ? DATABASE_URL : {
+  connection: {
     host : DB_HOST,
     port : DB_PORT,
     user : DB_USER,
