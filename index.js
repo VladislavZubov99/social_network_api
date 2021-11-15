e802948f39175f41bbb474671adcb3652724d2b3
Vladislav Zubov <vladislav.zubov.20@gmail.com>
2021-11-15 10:41:29 +0300

    Added welcome message

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use('/api', userRouter);
 app.use('/api', postRouter);
 app.use('/api', commentRouter);
+app.get('/', (req, res) => res.send("Welcome to API for social_network blog"));
 
 app.listen(PORT, () => console.log('server started on', PORT, 'port'))
